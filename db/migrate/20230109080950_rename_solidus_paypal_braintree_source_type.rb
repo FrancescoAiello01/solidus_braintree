e1216039bbebeb3931ca43ee75b71f23941f7630
George Mendoza <gsmendoza@gmail.com>
2023-01-17 17:00:34 +0800

    Update InstallGenerator to update to SolidusBraintree when applicable
    * Renames engine route and asset file paths.
    * Adds migration to update records referring to SolidusPaypalBraintree.

---
+++ db/migrate/20230109080950_rename_solidus_paypal_braintree_source_type.rb
@@ -0,0 +1,16 @@
+class RenameSolidusPaypalBraintreeSourceType < ActiveRecord::Migration[6.1]
+  def up
+    Spree::Payment.where(source_type: 'SolidusPaypalBraintree::Source')
+      .update_all("source_type = 'SolidusBraintree::Source'")
+
+    Spree::WalletPaymentSource.where(payment_source_type: 'SolidusPaypalBraintree::Source')
+      .update_all("payment_source_type = 'SolidusBraintree::Source'")
+
+    Spree::PaymentMethod.where('type = ?', 'SolidusPaypalBraintree::Gateway')
+      .update_all("type = 'SolidusBraintree::Gateway'")
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
